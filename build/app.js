ec7cce165a3bec1175a8845a4effb9b2ce5b6374
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-10-24 11:41:32 +0530

    Add get and post method for TODO List

--- build/app.js
+++ build/app.js
@@ -6,8 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 3000;
+// parsing the body (json)
+app.use(express_1.default.json());
+// parsing the body (urlencoded-formdata)
+app.use(express_1.default.urlencoded({ extended: false }));
+const todoList = [];
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello New Project");
+});
+app.get("/todo", (req, res) => {
+    res.send(todoList);
+});
+app.post("/todo", (req, res) => {
+    todoList.push(req.body);
+    res.status(201).send("Successfully create todo");
 });
 app.listen(PORT, () => {
     console.log(`App is running on port:${PORT}`);
--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,30 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 const PORT = 3000;
 
+// parsing the body (json)
+app.use(express.json());
+// parsing the body (urlencoded-formdata)
+app.use(express.urlencoded({ extended: false }));
+
+interface ToDo {
+  id: number;
+  title: string;
+  important: boolean;
+}
+
+const todoList: ToDo[] = [];
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello New Project");
+});
+
+app.get("/todo", (req: Request, res: Response) => {
+  res.send(todoList);
+});
+
+app.post("/todo", (req: Request, res: Response) => {
+  todoList.push(req.body);
+  res.status(201).send("Successfully created todo");
 });
 
 app.listen(PORT, () => {
