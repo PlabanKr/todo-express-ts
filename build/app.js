3bd599d9ad52761af722aa851b62c8106b39b37a
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2022-10-28 18:17:40 +0530

    Refactor everything to use mongodb

--- build/app.js
+++ build/app.js
@@ -1,43 +1,80 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = require("mongoose");
+const todo_1 = require("./models/todo");
 const app = (0, express_1.default)();
+const DB_URL = "mongodb://localhost:27017/todo-express-ts";
 const PORT = 3000;
+(0, mongoose_1.connect)(DB_URL)
+    .then(() => console.log("Database connected"))
+    .catch((err) => console.error(err));
 // parsing the body (json)
 app.use(express_1.default.json());
 // parsing the body (urlencoded-formdata)
 app.use(express_1.default.urlencoded({ extended: false }));
-const todoList = [];
 app.get("/", (req, res) => {
     res.send("Hello New Project");
 });
-app.get("/todo", (req, res) => {
-    res.send(todoList);
-});
-app.post("/todo", (req, res) => {
-    todoList.push(req.body);
-    res.status(201).send("Successfully created todo");
-});
-app.put("/todo", (req, res) => {
-    todoList.map((todo) => {
-        if (todo.id == req.body.id) {
-            todo.title = req.body.title;
-            todo.important = req.body.important;
-            return res.send(todo);
-        }
-    });
-    return res.send({
-        error: "ToDo id does not exist!"
-    });
-});
-app.delete("/todo", (req, res) => {
-    const i = todoList.findIndex((ele) => ele.id == req.body.id);
-    todoList.splice(i, 1);
-    return res.send(todoList);
-});
+app.get("/todo", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const tasks = yield todo_1.Todo.find({});
+        res.status(200).json(tasks);
+    }
+    catch (e) {
+        res.status(400).json({
+            error: e
+        });
+    }
+}));
+app.post("/todo", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const todo = new todo_1.Todo(req.body);
+        yield todo.save();
+        res.status(201).json(todo);
+    }
+    catch (e) {
+        res.status(400).json({
+            error: e
+        });
+    }
+}));
+app.put("/todo/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const todo = yield todo_1.Todo.findByIdAndUpdate(req.params.id, Object.assign({}, req.body));
+        res.status(200).json(todo);
+    }
+    catch (e) {
+        res.status(400).json({
+            error: e
+        });
+    }
+}));
+app.delete("/todo/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield todo_1.Todo.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            message: "Todo was successfully deleted"
+        });
+    }
+    catch (e) {
+        res.status(400).json({
+            error: e
+        });
+    }
+}));
 app.listen(PORT, () => {
     console.log(`App is running on port:${PORT}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,52 +1,69 @@
 import express, { Application, Request, Response } from "express";
+import { connect } from "mongoose";
+import { Todo } from "./models/todo";
 
 const app: Application = express();
+const DB_URL = "mongodb://localhost:27017/todo-express-ts";
 const PORT = 3000;
+
+connect(DB_URL)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error(err));
 
 // parsing the body (json)
 app.use(express.json());
 // parsing the body (urlencoded-formdata)
 app.use(express.urlencoded({ extended: false }));
 
-interface ToDo {
-  id: number;
-  title: string;
-  important: boolean;
-}
-
-const todoList: ToDo[] = [];
-
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello New Project");
 });
 
-app.get("/todo", (req: Request, res: Response) => {
-  res.send(todoList);
+app.get("/todo", async (req: Request, res: Response) => {
+  try {
+    const tasks = await Todo.find({});
+    res.status(200).json(tasks);
+  } catch (e) {
+    res.status(400).json({
+      error: e
+    });
+  }
 });
 
-app.post("/todo", (req: Request, res: Response) => {
-  todoList.push(req.body);
-  res.status(201).send("Successfully created todo");
+app.post("/todo", async (req: Request, res: Response) => {
+  try {
+    const todo = new Todo(req.body);
+    await todo.save();
+    res.status(201).json(todo);
+  } catch (e) {
+    res.status(400).json({
+      error: e
+    });
+  }
 });
 
-app.put("/todo", (req: Request, res: Response) => {
-  todoList.map((todo) => {
-    if (todo.id == req.body.id) {
-      todo.title = req.body.title;
-      todo.important = req.body.important;
-      return res.send(todo);
-    }
-  });
-
-  return res.send({
-    error: "ToDo id does not exist!"
-  });
+app.put("/todo/:id", async (req: Request, res: Response) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, { ...req.body });
+    res.status(200).json(todo);
+  } catch (e) {
+    res.status(400).json({
+      error: e
+    });
+  }
 });
 
-app.delete("/todo", (req: Request, res: Response) => {
-  const i = todoList.findIndex((ele) => ele.id == req.body.id);
-  todoList.splice(i, 1);
-  return res.send(todoList);
+app.delete("/todo/:id", async (req: Request, res: Response) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Todo was successfully deleted"
+    });
+  } catch (e) {
+    res.status(400).json({
+      error: e
+    });
+  }
 });
 
 app.listen(PORT, () => {
